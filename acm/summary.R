01f82fb1b7b0fc6f3dd5919da0b836adc3c7f14a
Axtux <axtux@hotmail.com>
2017-06-07 21:50:53 +0200

    see effect on classes

--- acm/summary.R
+++ acm/summary.R
@@ -2,4 +2,23 @@
 print("reading data file...")
 data = read.csv("../data/winequality-red.csv", sep=";")
 
-summary(data)
+# min, max and mean
+print(summary(data))
+
+# print 32 classes to help identify classes limits
+classes = data.frame(data)
+for(n in names(data)) {
+  # Brooks-Carruthers formula classes = 5 * log(length(data), 10)
+  
+  # same interval classes
+  classes[,n] = cut(data[,n], 5, include.lowest=TRUE, right=FALSE)
+  
+  # same number classes
+  classes[,n] = cut(data[,n], unique(quantile(data[,n])), include.lowest=TRUE, right=FALSE)
+  
+  print(n)
+  print(summary(classes[,n]))
+}
+
+
+
